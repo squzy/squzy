b5a850f1cfb7fab55fbdce86bd9b37f00a3c705c
Iurii Panarin <iurii.panarin@mcmakler.de>
2019-11-26 00:05:16 +0100

    tests(storage): add tests

--- apps/squzy/storage/external_storage.go
+++ apps/squzy/storage/external_storage.go
@@ -5,11 +5,16 @@ import (
 	"errors"
 	clientPb "github.com/squzy/squzy_generated/generated/logger"
 	"squzy/apps/internal/job"
+	"time"
 )
 
 type externalStorage struct {
 	client clientPb.LoggerClient
 }
+
+const (
+	loggerConnTimeout = time.Second * 5
+)
 
 var (
 	connectionExternalStorageError = errors.New("CANT_CONNECT_TO_EXTERNAL_STORAGE")
@@ -21,11 +26,13 @@ func NewExternalStorage(client clientPb.LoggerClient) Storage {
 }
 
 func (s *externalStorage) Write(id string, log job.CheckError) error {
+
 	req := &clientPb.SendLogMessageRequest{
 		Log: log.GetLogData(),
 	}
-
-	res, err := s.client.SendLogMessage(context.Background(), req)
+	ctx, cancel := context.WithTimeout(context.Background(), loggerConnTimeout)
+	defer cancel()
+	res, err := s.client.SendLogMessage(ctx, req)
 	if err != nil {
 		return connectionExternalStorageError
 	}
---
+++ apps/squzy/storage/external_storage_test.go
@@ -0,0 +1,70 @@
+package storage
+
+import (
+	"context"
+	"errors"
+	squzy_logger_v1_service "github.com/squzy/squzy_generated/generated/logger"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"testing"
+)
+
+type client_mock_error struct {
+
+}
+
+type client_mock_error_not_save struct {
+
+}
+
+func (c client_mock_error_not_save) SendLogMessage(ctx context.Context, in *squzy_logger_v1_service.SendLogMessageRequest, opts ...grpc.CallOption) (*squzy_logger_v1_service.SendLogMessageResponse, error) {
+	return &squzy_logger_v1_service.SendLogMessageResponse{
+		Success: false,
+	}, nil
+}
+
+type client_mock struct {
+
+}
+
+func (c client_mock) SendLogMessage(ctx context.Context, in *squzy_logger_v1_service.SendLogMessageRequest, opts ...grpc.CallOption) (*squzy_logger_v1_service.SendLogMessageResponse, error) {
+	return &squzy_logger_v1_service.SendLogMessageResponse{
+		Success: true,
+	}, nil
+}
+
+type mock struct {
+
+}
+
+func (m mock) GetLogData() *squzy_logger_v1_service.Log {
+	return &squzy_logger_v1_service.Log{}
+}
+
+func (c client_mock_error) SendLogMessage(ctx context.Context, in *squzy_logger_v1_service.SendLogMessageRequest, opts ...grpc.CallOption) (*squzy_logger_v1_service.SendLogMessageResponse, error) {
+	return nil, errors.New("Error")
+}
+
+func TestNewExternalStorage(t *testing.T) {
+	t.Run("Test: Create new storage", func(t *testing.T) {
+		s := NewExternalStorage(nil)
+		assert.Implements(t, (*Storage)(nil), s)
+	})
+}
+
+func TestExternalStorage_Write(t *testing.T) {
+	t.Run("Should: return connectionExternalStorageError", func(t *testing.T) {
+		s := NewExternalStorage(&client_mock_error{})
+		assert.Equal(t, connectionExternalStorageError, s.Write("", &mock{}))
+	})
+
+	t.Run("Should: return nil", func(t *testing.T) {
+		s := NewExternalStorage(&client_mock{})
+		assert.Equal(t, nil, s.Write("", &mock{}))
+	})
+
+	t.Run("Should: return storageNotSaveLog", func(t *testing.T) {
+		s := NewExternalStorage(&client_mock_error_not_save{})
+		assert.Equal(t, storageNotSaveLog, s.Write("", &mock{}))
+	})
+}
