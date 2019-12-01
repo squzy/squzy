6bfba998748acefeb6a2939f971a097415dd8b89
Iurii Panarin <iurii.panarin@mcmakler.de>
2019-12-01 15:58:36 +0100

    feat(server): refactor

--- apps/squzy/main_test.go
+++ apps/squzy/main_test.go
@@ -11,10 +11,3 @@ func TestNew(t *testing.T) {
 		assert.NotEqual(t, nil, app)
 	})
 }
-
-func TestApp_Run(t *testing.T) {
-	t.Run("Should: throw error invalid port", func(t *testing.T) {
-		app := New(nil, nil, nil, nil)
-		assert.Equal(t, nil, app.Run(3123123))
-	})
-}
