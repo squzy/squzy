27d1e542fb67535b8ad3ba4b7558ebe67d546a6c
Iurii Panarin <iurii.panarin@mcmakler.de>
2019-12-01 20:13:11 +0100

    feat(tests): add tests

---
+++ apps/squzy/application_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	t.Run("Should: Create new application", func(t *testing.T) {
+		app := New(nil, nil, nil, nil)
+		assert.NotEqual(t, nil, app)
+	})
+}
+
+func TestApp_Run(t *testing.T) {
+	t.Run("Should: return error", func(t *testing.T) {
+		app := New(nil, nil, nil, nil)
+		assert.NotEqual(t, nil, app.Run(1244214))
+	})
+	t.Run("Should: not return error", func(t *testing.T) {
+		app := New(nil, nil, nil, nil)
+		go func() {
+			app.Run(11111)
+		}()
+		_, err := net.Dial("tcp", "localhost:11111")
+		assert.Equal(t, nil, err)
+	})
+}
