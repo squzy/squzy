0c32290daa2b8b9ef94f7e28596592951cfa058c
Iurii Panarin <tduble94@gmail.com>
2020-05-05 01:20:13 +0200

    ci(build)

--- build.bash
+++ build.bash
@@ -21,7 +21,6 @@ do
     if [ $GOOS = "windows" ]; then
         output_name+='.exe'
     fi
-    echo "-X $package_path/version.Version=$version_inject"
     env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name -ldflags "-s -w -X squzy/apps/$package_path/version.Version=$version_inject"  apps/$package_path/main.go
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
