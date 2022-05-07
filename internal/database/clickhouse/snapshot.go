e16feb811225dafbab76dd807b0d6fdd46df1d7e
peterjasc <peeter.kokk@gmail.com>
2022-05-07 20:42:44 +0200

    remove logs

--- internal/database/clickhouse/snapshot.go
+++ internal/database/clickhouse/snapshot.go
@@ -134,11 +134,6 @@ func (c *Clickhouse) GetSnapshots(request *apiPb.GetSchedulerInformationRequest)
 
 func (c *Clickhouse) countSnapshots(request *apiPb.GetSchedulerInformationRequest, timeFrom int64, timeTo int64) (int64, error) {
 	var count int64
-	fmt.Println(fmt.Sprintf(`SELECT count(*) FROM %s WHERE %s AND (%s) AND %s LIMIT 1`,
-		dbSnapshotCollection,
-		snapshotSchedulerIdString,
-		snapshotMetaStartTimeFilterString,
-		getCodeString(request.Status)))
 	rows, err := c.Db.Query(fmt.Sprintf(`SELECT count(*) FROM "%s" WHERE %s AND (%s) AND %s LIMIT 1`,
 		dbSnapshotCollection,
 		snapshotSchedulerIdString,
